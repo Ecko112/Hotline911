1b00619c3816482f22d4e218637426f31951e0f7
Ecko112 <houssa.louis@gmail.com>
2018-12-07 00:05:35 +0100

    ras

--- NOTRUF/class_menu.py
+++ NOTRUF/class_menu.py
@@ -1,6 +1,9 @@
 import pygame
 # import math
+pygame.font.init()
 
+X = 0
+Y= 1
 
 class Menu:
     ################
@@ -9,12 +12,18 @@ class Menu:
     # [DEV]
     # FORCE START LEVEL
     FORCE_START_LEVEL = pygame.K_BACKSPACE
+    # Polices
+    title_font = pygame.font.SysFont('monospace', 112, True)
 
     def __init__(self, MAIN):
         # INIT
         self.MAIN = MAIN
         self.SCREEN = self.MAIN.SCREEN
-        
+        self.SCREEN_RESOLUTION = self.MAIN.SCREEN_RESOLUTION
+        # Set title
+        self.title = self.title_font.render('HOTLINE911', True, (255, 0, 0))
+        self.title_size = self.title_font.size('HOTLINE911')
+
     def loop_menu(self):
         self.process_input()
         self.paint_menu()
@@ -22,6 +31,8 @@ class Menu:
     def paint_menu(self):
         # Background
         self.SCREEN.fill((100, 100, 100))
+        # Title
+        self.SCREEN.blit(self.title, ((self.SCREEN_RESOLUTION[X]-self.title_size[X])//2, (self.SCREEN_RESOLUTION[Y])//4))
         # Update screen
         pygame.display.flip()
 
