e53ff94399926937dbab63ff3cb9244c8dd7cff7
Ecko112 <houssa.louis@gmail.com>
2018-12-09 18:26:58 +0100

    ras

--- NOTRUF/class_furniture.py
+++ NOTRUF/class_furniture.py
@@ -25,13 +25,10 @@ class Furniture:
         self.pos = pos
         self.length = length
         self.width = width
-        self.Rect = pygame.Rect(0, 0, self.length, self.width)
-        self.Rect.center = self.pos
+        self.Rect = self.influence_Rect = pygame.Rect(0, 0, self.length, self.width)
+        self.Rect.center = self.influence_Rect.center = self.pos
 
-        self.influence_rect = pygame.Rect(0, 0, self.length, self.width)
-        self.influence_rect.center = self.pos
-
-        self.influence_rad = self.ROOM.GRID_SIZE
+        self.influence_rad = self.STRUCTURE.GRID_SIZE
         self.influence_max = self.ROOM.GRID_SIZE * 5 / 2
         self.influence = 1/100
         self.influence_room = self.influence
@@ -43,15 +40,11 @@ class Furniture:
     def paint_furniture(self):
         pygame.draw.rect(self.SCREEN, self.texture, self.Rect)
         # [DEV] DRAW RADIATION ZONE
-        pygame.draw.rect(self.SCREEN, (0, 0, 0), self.influence_rect, 2)
+        pygame.draw.rect(self.SCREEN, (0, 0, 0), self.influence_Rect, 2)
 
     def ignite(self):
-        self.burning = True
-        self.temp = self.ignition_tresh
-        self.influence_rect = pygame.Rect(0, 0, self.length + self.influence_rad, self.width + self.influence_rad)
-        self.influence_rect.center = self.pos
-        self.influence_rad = 0
         self.texture = (255, 0, 0)
+        pass
 
     def isHeatingUp(self):
         for other in self.ROOM.Furniture:
@@ -59,27 +52,7 @@ class Furniture:
                 return self.Rect.collidelist([other.influence_rect]) != -1
 
     def isCooledDown(self):
-        # for water in self.LEVEL.
         pass
 
     def burn(self):
-        if self.burning:
-            self.temp += 1 / 30
-            self.ROOM.temp += self.influence_room
-        elif self.isHeatingUp():
-            self.temp += 1 / 100
-        if self.temp <= self.ignition_tresh:
-            self.influence_rad = 0
-        elif int(self.temp) == self.ignition_tresh:
-            self.ignite()
-        elif self.ignition_tresh <= self.temp < 400:
-            if self.influence_rad < self.influence_max:
-                self.influence_rad += 1/50
-                self.influence_room += self.temp // 100
-            else:
-                self.influence_rad = self.influence_max
-        elif 400 <= self.temp:
-            self.influence_rad = self.influence_max
-
-        self.influence_rect = pygame.Rect(0, 0, self.length + self.influence_rad, self.width + self.influence_rad)
-        self.influence_rect.center = self.pos
+        pass
